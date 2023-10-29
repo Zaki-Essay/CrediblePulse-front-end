cbd47a47d8a625455c795eedd9f53cdd999d8ab7
Zakariae ESSAIYDY <zakaressaiydy@gmail.com>
2023-10-29 16:03:54 +0100

    eslint and prettier added to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import './App.css'
+import "./App.css";
 function App() {
-
   return (
     <>
-      <div className="bg-blue-500 text-white p-4">
-      Hello, Tailwind CSS!
-    </div>
+      <div className="bg-blue-500 text-white p-4">Hello, Tailwind CSS!</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
